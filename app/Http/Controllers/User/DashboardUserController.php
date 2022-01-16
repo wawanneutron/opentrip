c4eda7188d23520f67322a723914249ec75be3ca
wawanneutron <wawanneutron1331@gmail.com>
2022-01-17 00:08:24 +0700

    dashboard user

---
+++ app/Http/Controllers/User/DashboardUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardUserController extends Controller
+{
+    public function index()
+    {
+        return view('pages.user.dashboard');
+    }
+
+    public function historyTransaction()
+    {
+        $historytTransaction = Transaction::latest()
+            ->whereUsersId(Auth::user()->id)
+            ->get();
+        return view('pages.user.transaction.index', [
+            'histories' => $historytTransaction
+        ]);
+    }
+
+    public function detailTransaction($id)
+    {
+        $detailTransaction = Transaction::with('details')->findOrFail($id);
+        return view('pages.user.transaction.detail', [
+            'item' => $detailTransaction
+        ]);
+    }
+}
---
+++ resources/views/pages/user/transaction/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.user')
+
+@section('content')
+    <!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Transactions</h1>
+  </div>
+  <div class="row">
+    <div class="col-lg">
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-bordered table-responsive-sm" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Open Trip</th>
+                <th>Member / User</th>
+                <th>Harga Open Trip</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            @forelse ($histories as $item)
+              <tr>
+                <td> {{ $item->id }} </td>
+                <td> {{ $item->travel_package->title }} </td>
+                <td> {{ $item->user->name }} </td>
+                <td> {{ moneyFormat($item->transaction_total )}} </td>
+                <td> {{ $item->transaction_status }} </td>
+                <td>
+                  <a href="{{ route('user.detail-transaction', $item->id) }}" class="btn btn-primary">
+                    <i class="fa fa-eye"></i>
+                  </a>
+                </td>
+              </tr>
+              @empty
+                  <tr>
+                    <td colspan="7" class="text-center">
+                      Data Kosong
+                    </td>
+                  </tr>
+            @endforelse
+            </tbody>
+          </table>
+        </div>
+    </div>
+    </div>
+    
+  </div>
+
+
+</div>
+
+@endsection
