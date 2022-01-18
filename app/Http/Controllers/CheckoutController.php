d3fadc4ee236409f73f5ab6480bd743cabd1e174
wawanneutron <wawanneutron1331@gmail.com>
2022-01-19 00:48:13 +0700

    validation quota

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -109,41 +109,48 @@ class CheckoutController extends Controller
 
     public function succses(Request $request, $id)
     {
-        $transaction = Transaction::with([
-            'details', 'travel_package.galleries',
-            'user'
-        ])->findOrFail($id);
-        $transaction->transaction_status = 'PENDING';
+        $travel_package_id  = Transaction::with(['travel_package'])->find($id);
+        $cek_kuota          = $travel_package_id->travel_package->quota;
 
-        $transaction->save();
+        if ($cek_kuota <= 0) {
+            return 'Kuota habis';
+        } else {
+            $transaction = Transaction::with([
+                'details', 'travel_package.galleries',
+                'user'
+            ])->findOrFail($id);
+            $transaction->transaction_status = 'PENDING';
 
-        // Set Configurasi Midtrans
-        Config::$serverKey = config('midtrans.serverKey');
-        Config::$isProduction = config('midtrans.isProduction');
-        Config::$isSanitized = config('midtrans.isSanitized');
-        Config::$is3ds = config('midtrans.is3ds');
+            $transaction->save();
 
-        // Buat array untuk dikirim ke midtrans
-        $midtrans_params = [
-            'transaction_details' => [
-                'order_id' => $transaction->kd_transaction,
-                'gross_amount' => (int) $transaction->transaction_total
-            ],
-            'customer_details' => [
-                'first_name' => $transaction->user->name,
-                'email' => $transaction->user->email,
-            ],
-            'vtweb' => []
-        ];
+            // Set Configurasi Midtrans
+            Config::$serverKey = config('midtrans.serverKey');
+            Config::$isProduction = config('midtrans.isProduction');
+            Config::$isSanitized = config('midtrans.isSanitized');
+            Config::$is3ds = config('midtrans.is3ds');
 
-        try {
-            //Get Snap Payment Page URL
-            $paymentUrl = Snap::createTransaction($midtrans_params)->redirect_url;
+            // Buat array untuk dikirim ke midtrans
+            $midtrans_params = [
+                'transaction_details' => [
+                    'order_id' => $transaction->kd_transaction,
+                    'gross_amount' => (int) $transaction->transaction_total
+                ],
+                'customer_details' => [
+                    'first_name' => $transaction->user->name,
+                    'email' => $transaction->user->email,
+                ],
+                'vtweb' => []
+            ];
 
-            // Redirect to Snap Payment Page
-            return redirect($paymentUrl);
-        } catch (Exception $e) {
-            echo $e->getMessage();
+            try {
+                //Get Snap Payment Page URL
+                $paymentUrl = Snap::createTransaction($midtrans_params)->redirect_url;
+
+                // Redirect to Snap Payment Page
+                return redirect($paymentUrl);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
     }
 }
