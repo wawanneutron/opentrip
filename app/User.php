67b0c0a99a9535501f362cd9dcded90709a93169
wawanneutron <wawanneutron1331@gmail.com>
2022-01-18 21:35:24 +0700

    add field

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'username', 'email', 'password',
+        'id', 'userid', 'name', 'username', 'email', 'password',
     ];
 
     /**
